2e0ee3a53f9087aac9ae6d51b069f5bc555bac4b
Daniel Ding <41363844+BioniCosmos@users.noreply.github.com>
2026-06-05 14:17:45 +0800

    Revert "Add Cloudflare Workers configuration"
    This reverts commit 337bea1fc3b9207258d80858371562f39cdbfc15.

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,5 @@
-import cloudflare from '@astrojs/cloudflare'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({
-  vite: { plugins: [tailwindcss()] },
-  adapter: cloudflare(),
-})
+export default defineConfig({ vite: { plugins: [tailwindcss()] } })
