3215b6b429275bb8c6e46f69f328b72015782910
Daniel Ding <41363844+BioniCosmos@users.noreply.github.com>
2026-06-05 13:57:01 +0800

    Format

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,9 @@
+import cloudflare from '@astrojs/cloudflare'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
-
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   vite: { plugins: [tailwindcss()] },
-  adapter: cloudflare()
+  adapter: cloudflare(),
 })
